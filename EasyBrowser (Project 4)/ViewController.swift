816155776a97e75a56a5f924495e54080602c2ec
Sinan KULEN <sinan.kulen@gmail.com>
2021-07-07 11:34:47 +0300

    Initial Commit

---
+++ EasyBrowser (Project 4)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyBrowser (Project 4)
+//
+//  Created by Sinan Kulen on 7.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
